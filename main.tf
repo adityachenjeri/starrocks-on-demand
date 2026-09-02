a399bdfb71cafa9fda6f4756ea6b16140f3bc798
Aditya sai chenjeri <adityasai0330@gmail.com>
2026-09-02 19:03:27 +0530

    Change instance type to t3.micro to comply with lab SCP rules

--- main.tf
+++ main.tf
@@ -11,7 +11,24 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# 1. Firewall Rule (Security Group)
+# 1. Fetch latest official Ubuntu 22.04 AMI dynamically
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical owner ID
+}
+
+# 2. Firewall Rule (Security Group)
 resource "aws_security_group" "starrocks_sg" {
   name        = "starrocks-on-demand-sg"
   description = "Allow MySQL access to StarRocks"
@@ -23,7 +40,6 @@ resource "aws_security_group" "starrocks_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # StarRocks MySQL Protocol Port
   ingress {
     from_port   = 9030
     to_port     = 9030
@@ -39,14 +55,13 @@ resource "aws_security_group" "starrocks_sg" {
   }
 }
 
-# 2. AWS EC2 Server
+# 3. AWS EC2 Server (Using allowed t3.micro instance type)
 resource "aws_instance" "starrocks_server" {
-  ami           = "ami-0c7217cdde317cfec" # Ubuntu 22.04 LTS (us-east-1)
-  instance_type = "t3.large"              # 2 vCPU, 8GB RAM
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro" # Changed from t3.large to pass KodeKloud SCP restrictions
 
   security_groups = [aws_security_group.starrocks_sg.name]
 
-  # User Data: Automatically starts StarRocks in Docker on launch
   user_data = <<-EOF
               #!/bin/bash
               apt-get update -y
@@ -54,7 +69,7 @@ resource "aws_instance" "starrocks_server" {
               systemctl start docker
               systemctl enable docker
 
-              # Run all-in-one StarRocks container directly
+              # Run all-in-one StarRocks container
               docker run -p 9030:9030 -p 8030:8030 -p 8040:8040 -itd --name starrocks starrocks/all-in-1-ubuntu
               EOF
 
@@ -63,7 +78,7 @@ resource "aws_instance" "starrocks_server" {
   }
 }
 
-# 3. Connection Output Link for Data Team
+# 4. Connection Output
 output "data_team_connection_string" {
   value       = "mysql -h ${aws_instance.starrocks_server.public_ip} -P 9030 -u root"
   description = "Share this connection command with the Data Analysis team"
